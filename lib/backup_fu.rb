8395ef5cae3bbffc85f06789ede53487ddf9d9f8
Samuel Goebert <hello@bigcurl.de>
2009-02-07 13:44:41 +0100

    Commented out the s3 cleanup code. It is not ported to right_aws yet.

--- lib/backup_fu.rb
+++ lib/backup_fu.rb
@@ -184,18 +184,20 @@ class BackupFu
 
     end
     
-    # Deleting stuff from Amazon too
-    establish_s3_connection
-    backups = AWS::S3::Bucket.find(@fu_conf[:s3_bucket], :prefix => @fu_conf[:app_name])
-    if count >= backups.objects.length
-      puts "no old backups to cleanup"
-    else
-      puts "keeping #{count} of #{backups.objects.length} S3 backups"
-      sorted = backups.objects.sort {|a,b| a.key <=> b.key}
-      (backups.objects.length-count).times do
-        backups.objects.first.delete
-      end
-    end
+    # This part is not portet to right_aws.
+    #
+    # # Deleting stuff from Amazon too
+    # establish_s3_connection
+    # backups = AWS::S3::Bucket.find(@fu_conf[:s3_bucket], :prefix => @fu_conf[:app_name])
+    # if count >= backups.objects.length
+    #   puts "no old backups to cleanup"
+    # else
+    #   puts "keeping #{count} of #{backups.objects.length} S3 backups"
+    #   sorted = backups.objects.sort {|a,b| a.key <=> b.key}
+    #   (backups.objects.length-count).times do
+    #     backups.objects.first.delete
+    #   end
+    # end
   end
   
   private
